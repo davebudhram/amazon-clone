293df476baef361c6f1e5d99afd006c61f951c98
Dave Budhram <davebudhram18@gmail.com>
2023-09-04 14:53:11 -0400

    Move function into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,26 @@
-const cart = [];
+
+export const cart = [];
+
+/**
+ * Adds item to card using productId given
+ * @param {*} productId 
+ */
+export function addToCart(productId){
+  let matchingItem;
+  cart.forEach((cartItem) =>{
+    if (productId === cartItem.productId){
+      matchingItem = cartItem;
+    }
+  });
+  
+  if (matchingItem){
+    matchingItem.quantity +=1;
+  }
+  else{
+    cart.push({
+      productId: productId,
+      quantity: 1})
+  };
+}
+
+
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -59,31 +59,29 @@ products.forEach((value, index) => {
 
 const productsGrid = document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+
+
+
+/**
+ * Updates the cart quantity
+ */
+function updateCartQuantity(){
+  let cartQuantity = 0;
+      cart.forEach((cartItem)=>{
+        cartQuantity += cartItem.quantity;
+      });
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+/*
+  Button to add products to cart
+*/
 document.querySelectorAll('.js-add-to-cart').forEach(
   (button) =>{
     button.addEventListener('click', ()=>{
       const productId = button.dataset.productId;
-
-      let matchingItem;
-      cart.forEach((item) =>{
-        if (productId === item.productId){
-          matchingItem = item;
-        }
-      });
+      addToCart(productId);
+      updateCartQuantity();
       
-      if (matchingItem){
-        matchingItem.quantity +=1;
-      }
-      else{
-        cart.push({
-          productId: productId,
-          quantity: 1})
-      };
-      let cartQuantity = 0;
-      cart.forEach((item)=>{
-        cartQuantity += item.quantity;
-      });
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     })
   }
 )
