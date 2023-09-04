ead5eba132b76a41a23b2eb9cfc6e5b96ba7f8a3
Dave Budhram <davebudhram18@gmail.com>
2023-09-04 14:31:26 -0400

    Using modules and imports

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 let productsHTML = '';
 
@@ -77,7 +79,12 @@ document.querySelectorAll('.js-add-to-cart').forEach(
           productId: productId,
           quantity: 1})
       };
-      console.log(cart);
+      let cartQuantity = 0;
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     })
   }
 )
+
